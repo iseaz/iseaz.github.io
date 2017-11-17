0e5c92fe9883f6ab053629dc8fb3803d9a289f70
Apisit Asavatappitak <apisit@ADAPTERDIGITAL.LOCAL>
2017-11-17 18:06:42 +0700

    change to vue

--- pwa/ecma/app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('https://iseaz.github.io/pwa/sw.js').then(() => {
-		console.log('Service worker registered!')
-	}).catch(err => {
-		console.log(err)
-	})
-}
--- pwa/sw.js
+++ pwa/sw.js
@@ -1,14 +1,58 @@
-self.addEventListener('install', event => {
-	console.log('Service worker installed')
+let CACHE_STATIC_NAME = 'static-v1'
+let CACHE_DYNAMIC_NAME = 'dynamic-v1'
+let STATIC_FILES = [
+	'./',
+	'./index.html',
+	'offline.html',
+	'./dist/build.js'
+]
 
+self.addEventListener('install', event => {
 	event.waitUntil(
-		caches.open('static').then(cache => {
-			cache.addAll([
-				'https://iseaz.github.io/pwa',
-				'https://iseaz.github.io/pwa/',
-				'https://iseaz.github.io/pwa/index.html',
-				'https://iseaz.github.io/pwa/ecma/app.js'
-			])
-		})
+		caches.open(CACHE_STATIC_NAME)
+			.then(cache => {
+				cache.addAll(STATIC_FILES)
+			})
+	)
+})
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys()
+			.then(keyList => {
+				return Promise.all(keyList.map(key => {
+					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+						return caches.delete(key)
+					}
+				}))
+			})
+	)
+
+	return self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request)
+			.then(response => {
+				if (response) {
+					return response
+				} else {
+					return fetch(event.request)
+						.then(res => {
+							return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+								cache.put(event.request.url, res.clone())
+
+								return res
+							})
+						})
+						.catch(err => {
+							return caches.open(CACHE_STATIC_NAME)
+								.then(cache => {
+									return cache.match('./offline.html')
+								})
+						})
+				}
+			})
 	)
 })
